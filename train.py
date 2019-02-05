edb4acd1f5557947d053b8685d50f9cc46959261
dresson <edresson1@gmail.com>
2019-02-05 12:00:17 -0200

    text2world

---
+++ train.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+# /usr/bin/python2
+from __future__ import print_function
+
+from tqdm import tqdm
+
+from data_load import get_batch, load_vocab
+from hyperparams import Hyperparams as hp
+from modules import *
+from networks import TextEnc, AudioEnc, AudioDec, Attention
+import tensorflow as tf
+from utils import *
+import sys
+
+
+class Graph:
+    def __init__(self, mode="train"):
+        '''
+        Args:
+          mode: Either "train" or "synthesize".
+        '''
+        # Load vocabulary
+        self.char2idx, self.idx2char = load_vocab()
+
+        # Set flag
+        training = True if mode=="train" else False
+
+        # Graph
+        # Data Feeding
+        ## L: Text. (B, N), int32
+        ## world: World Vocoder concatenate tensor.(B, 8*T/r, num_lf0+num_mgc+num_bap) float32
+        if mode=="train":
+            self.L, self.worlds, self.fnames, self.num_batch = get_batch()
+            self.prev_max_attentions = tf.ones(shape=(hp.B,), dtype=tf.int32)
+            self.gts = tf.convert_to_tensor(guided_attention())
+        else:  # Synthesize
+            self.L = tf.placeholder(tf.int32, shape=(None, None))
+            self.worlds = tf.placeholder(tf.float32, shape=(None, None, hp.num_bap+hp.num_lf0+hp.num_mgc))
+            self.prev_max_attentions = tf.placeholder(tf.int32, shape=(None,))
+            self.gts = tf.convert_to_tensor(guided_attention())
+
+        if not training:
+            with tf.variable_scope("Text2World"):
+                # Get S or decoder inputs. (B, 8*T/r, num_lf0+num_mgc+num_bap)
+                self.S = tf.concat((tf.zeros_like(self.worlds[:, :1, :]), self.worlds[:, :-1, :]), 1)
+
+                # Networks
+                with tf.variable_scope("TextEnc"):
+                    self.K, self.V = TextEnc(self.L, training=training)  # (N, Tx, e)
+
+                with tf.variable_scope("AudioEnc"):
+                    self.Q = AudioEnc(self.S, training=training)
+
+                with tf.variable_scope("Attention"):
+                    # R: (B, T/r, 2d)
+                    # alignments: (B, N, T/r)
+                    # max_attentions: (B,)
+                    self.R, self.alignments, self.max_attentions = Attention(self.Q, self.K, self.V,
+                                                                             mononotic_attention=(not training),
+                                                                             prev_max_attentions=self.prev_max_attentions)
+                with tf.variable_scope("AudioDec"):
+                    self.Y_logits, self.Y = AudioDec(self.R, training=training) # (B, T/r, num_lf0+num_mgc+num_bap)
+        
+
+
+        with tf.variable_scope("gs"):
+            self.global_step = tf.Variable(0, name='global_step', trainable=False)
+
+        if training:
+                # world L1 loss
+                self.loss_worlds = tf.reduce_mean(tf.abs(self.Y - self.worlds))
+
+                # world binary divergence loss
+                self.loss_bd1 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.Y_logits, labels=self.worlds))
+
+                # guided_attention loss
+                self.A = tf.pad(self.alignments, [(0, 0), (0, hp.max_N), (0, hp.max_T)], mode="CONSTANT", constant_values=-1.)[:, :hp.max_N, :hp.max_T]
+                self.attention_masks = tf.to_float(tf.not_equal(self.A, -1))
+                self.loss_att = tf.reduce_sum(tf.abs(self.A * self.gts) * self.attention_masks)
+                self.mask_sum = tf.reduce_sum(self.attention_masks)
+                self.loss_att /= self.mask_sum
+
+                # total loss
+                self.loss = self.loss_worlds + self.loss_bd1 + self.loss_att
+
+                tf.summary.scalar('train/loss_worlds', self.loss_worlds)
+                tf.summary.scalar('train/loss_bd1', self.loss_bd1)
+                #tf.summary.scalar('train/loss_att', self.loss_att)
+                tf.summary.image('train/world_gt', tf.expand_dims(tf.transpose(self.worlds[:1], [0, 2, 1]), -1))
+                tf.summary.image('train/world_hat', tf.expand_dims(tf.transpose(self.Y[:1], [0, 2, 1]), -1))
+
+                # Training Scheme
+                self.lr = learning_rate_decay(hp.lr, self.global_step)
+                self.optimizer = tf.train.AdamOptimizer(learning_rate=self.lr)
+                tf.summary.scalar("lr", self.lr)
+
+                ## gradient clipping
+                self.gvs = self.optimizer.compute_gradients(self.loss)
+                self.clipped = []
+                for grad, var in self.gvs:
+                    grad = tf.clip_by_value(grad, -1., 1.)
+                    self.clipped.append((grad, var))
+                    self.train_op = self.optimizer.apply_gradients(self.clipped, global_step=self.global_step)
+
+                # Summary
+                self.merged = tf.summary.merge_all()
+
+
+if __name__ == '__main__':
+    num = int(sys.argv[1])
+
+    g = Graph();print("Training Graph loaded")
+
+    logdir = hp.logdir + "-" + str(num)
+    sv = tf.train.Supervisor(logdir=logdir, save_model_secs=0, global_step=g.global_step)
+    sv.saver._max_to_keep=1000 # set max number checkpoint is save
+
+    with sv.managed_session() as sess:
+        while 1:
+            for _ in range(g.num_batch):
+                gs, _ = sess.run([g.global_step, g.train_op])
+
+                # Write checkpoint files at every 1k steps
+                if gs % 1000 == 0:
+                    sv.saver.save(sess, logdir + '/model_gs_{}'.format(str(gs // 1000).zfill(3) + "k"))
+                    print("processados "+str(gs)+" de "+str(hp.num_iterations))
+
+                    if num==1:
+                        # plot alignment
+                        alignments = sess.run(g.alignments)
+                        plot_alignment(alignments[0], str(gs // 1000).zfill(3) + "k", logdir)
+
+                # break
+                if gs > hp.num_iterations: 
+                    break
+
+    print("Done")
---
+++ utils.py
@@ -0,0 +1,307 @@
+# -*- coding: utf-8 -*-
+#/usr/bin/python2
+'''
+By kyubyong park. kbpark.linguist@gmail.com. 
+https://www.github.com/kyubyong/dc_tts
+'''
+from __future__ import print_function, division
+
+import numpy as np
+import librosa
+import os, copy
+import matplotlib
+matplotlib.use('pdf')
+import matplotlib.pyplot as plt
+from scipy import signal
+import codecs
+
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
+import copy
+from scipy import fftpack
+
+from hyperparams import Hyperparams as hp
+import tensorflow as tf
+
+
+
+import pysptk
+import soundfile as sf
+import pyworld as vocoder
+
+int16_max = 32768.0
+speed = 0.6
+frame_period = 7.1
+
+def f0_normalize(x):
+	return np.log(np.where(x == 0.0, 1.0, x)).astype(np.float32)
+
+def f0_denormalize(x):
+	return np.where(x == 0.0, 0.0, np.exp(x.astype(np.float64)))
+
+def sp_normalize(x):
+	sp = int16_max * np.sqrt(x)
+	return pysptk.sptk.mcep(sp.astype(np.float32), order=hp.num_mgc - 1, alpha=hp.mcep_alpha,
+				maxiter=0, threshold=0.001, etype=1, eps=1.0E-8, min_det=0.0, itype=3)
+
+def sp_denormalize(x):
+	sp = pysptk.sptk.mgc2sp(x.astype(np.float64), order=hp.num_mgc - 1,
+				alpha=hp.mcep_alpha, gamma=0.0, fftlen=hp.n_fft)
+	return np.square(sp / int16_max)
+
+def ap_normalize(x):
+	return x.astype(np.float32)
+
+def ap_denormalize(x, lf0):
+	for i in range(len(lf0)):
+		x[i] = np.where(lf0[i] == 0, np.zeros(x.shape[1]), x[i])
+	return x.astype(np.float64)
+
+
+def world_features_to_one_tensor(f0,sp,ap):
+    return np.column_stack((np.column_stack((np.array(f0),np.array(sp))),np.array(ap)))
+
+def tensor_to_world_features(tensor):
+    f0=[]
+    sp=[]
+    ap = []
+    sp_factor = hp.num_mgc+1
+    for i in range(len(tensor)):
+        f0.append(np.array(tensor[i][0]))
+        sp.append(np.array(tensor[i][1:sp_factor]))
+        ap.append(np.array(tensor[i][sp_factor:]))
+        
+    return np.array(f0),np.array(sp),np.array(ap)
+
+def wav2world(wavfile):
+    wav, fs = sf.read(wavfile)
+    #f0,sp,ap=vocoder.wav2world(wav,fs , hp.n_fft, ap_depth=hp.num_bap)
+    _f0_h, t_h = vocoder.dio(wav, fs, f0_floor=50.0, f0_ceil=600.0,
+                channels_in_octave=2,
+                frame_period=frame_period,
+                speed=speed)
+    
+    f0 = vocoder.stonemask(wav, _f0_h, t_h, fs)
+    sp = vocoder.cheaptrick(wav, f0, t_h, fs)
+    ap = vocoder.d4c(wav, f0, t_h, fs)
+    # feature normalization
+    lf0 = f0_normalize(f0)
+    mgc = sp_normalize(sp)
+    bap = ap_normalize(ap)
+    return np.array(world_features_to_one_tensor(lf0,mgc,bap))
+
+
+def world2wav(lf0, mgc, bap):
+	lf0 = np.where(lf0 < 1, 0.0, lf0)
+	f0 = f0_denormalize(lf0)
+	sp = sp_denormalize(mgc)
+	ap = ap_denormalize(bap, lf0)
+	print('features denomalize',lf0.shape,sp.shape,ap.shape)
+	wav = vocoder.synthesize(f0, sp, ap,hp.sr_dataset,vocoder.default_frame_period)
+	return wav
+
+
+def texts_to_phonemes(fpaths,texts,outputfile='texts-phoneme.csv',alphabet=False):
+    from PETRUS.g2p.g2p import G2PTranscriber
+    transcript = os.path.join(hp.data, outputfile)
+    if alphabet == True:
+        alphabet_list=[]
+        alpha=os.path.join(hp.data, 'phoneme-alphabet.csv')
+
+    transcript= codecs.open(transcript, 'w', 'utf-8')
+    for i in range(len(texts)):
+        words = texts[i].strip().lower().split(' ')
+        transcrito = [] 
+        for word in words:
+            g2p = G2PTranscriber(word, algorithm='silva')
+            transcription = g2p.transcriber()
+            transcrito.append(transcription)
+            if alphabet == True:
+                for caracter in transcription:
+                    if caracter not in alphabet_list:
+                        alphabet_list.append(caracter)
+
+        frase = str(fpaths[i])+'=='+"_".join(transcrito)+'\n'
+        transcript.write(frase)
+    if alphabet == True:
+        alphabet = codecs.open(alpha, 'w', 'utf-8')
+        for i in alphabet_list:
+            alphabet.write(i)
+
+
+def get_spectrograms(fpath):
+    '''Parse the wave file in `fpath` and
+    Returns normalized melspectrogram and linear spectrogram.
+
+    Args:
+      fpath: A string. The full path of a sound file.
+
+    Returns:
+      mel: A 2d array of shape (T, n_mels) and dtype of float32.
+     '''
+    # Loading sound file
+    y, sr = librosa.load(fpath, sr=hp.sr)
+
+    # Trimming
+    y, _ = librosa.effects.trim(y)
+
+    # Preemphasis
+    y = np.append(y[0], y[1:] - hp.preemphasis * y[:-1])
+ 
+    # stft
+    linear = librosa.stft(y=y,
+                          n_fft=hp.n_fft,
+                          hop_length=hp.hop_length,
+                          win_length=hp.win_length)
+
+    # magnitude spectrogram
+    mag = np.abs(linear)  # (1+n_fft//2, T)
+
+    # mel spectrogram
+    mel_basis = librosa.filters.mel(hp.sr, hp.n_fft, hp.n_mels)  # (n_mels, 1+n_fft//2)
+    mel = np.dot(mel_basis, mag)  # (n_mels, t)
+
+    # to decibel
+    mel = 20 * np.log10(np.maximum(1e-5, mel))
+    
+
+    # normalize
+    mel = np.clip((mel - hp.ref_db + hp.max_db) / hp.max_db, 1e-8, 1)
+    # Transpose
+    mel = mel.T.astype(np.float32)  # (T, n_mels)
+
+    return mel
+
+
+def spectrogram2wav(mag):
+    '''# Generate wave file from linear magnitude spectrogram
+
+    Args:
+      mag: A numpy array of (T, 1+n_fft//2)
+
+    Returns:
+      wav: A 1-D numpy array.
+    '''
+    # transpose
+    mag = mag.T
+
+    # de-noramlize
+    mag = (np.clip(mag, 0, 1) * hp.max_db) - hp.max_db + hp.ref_db
+    # to amplitude
+    mag = np.power(10.0, mag * 0.05)
+    if hp.vocoder == 'griffin_lim':
+        # wav reconstruction
+        wav = griffin_lim(mag**hp.power)
+    elif hp.vocoder == 'RTISI-LA' : # RTISI-LA
+        wav = iterate_invert_spectrogram(mag**hp.power)
+        
+    # de-preemphasis
+    wav = signal.lfilter([1], [1, -hp.preemphasis], wav)
+
+    # trim
+    wav, _ = librosa.effects.trim(wav)
+
+    return wav.astype(np.float32)
+
+def iterate_invert_spectrogram(X_s, n_iter=10, verbose=False,
+                               complex_input=False):
+    """
+    D. Griffin and J. Lim. Signal estimation from modified
+    short-time Fourier transform. IEEE Trans. Acoust. Speech
+    Signal Process., 32(2):236-243, 1984.
+    Malcolm Slaney, Daniel Naar and Richard F. Lyon. Auditory
+    Model Inversion for Sound Separation. Proc. IEEE-ICASSP,
+    Adelaide, 1994, II.77-80.
+    Xinglei Zhu, G. Beauregard, L. Wyse. Real-Time Signal
+    Estimation from Modified Short-Time Fourier Transform
+    Magnitude Spectra. IEEE Transactions on Audio Speech and
+    Language Processing, 08/2007.
+    """
+    reg = np.max(X_s) / 1E8
+    X_best = copy.deepcopy(X_s)
+    for i in range(n_iter):
+        if verbose:
+            print("Runnning iter %i" % i)
+        if i == 0 and not complex_input:
+            X_t = invert_spectrogram(X_best)
+        else:
+            # Calculate offset was False in the MATLAB version
+            # but in mine it massively improves the result
+            # Possible bug in my impl?
+            X_t = invert_spectrogram(X_best)
+        est = librosa.stft(X_t, hp.n_fft, hp.hop_length, win_length=hp.win_length)
+        phase = est / np.maximum(reg, np.abs(est))
+        phase = phase[:len(X_s)]
+        X_s = X_s[:len(phase)]
+        X_best = X_s * phase
+    X_t = invert_spectrogram(X_best)
+    return np.real(X_t)
+
+
+def griffin_lim(spectrogram):
+    '''Applies Griffin-Lim's raw.'''
+    X_best = copy.deepcopy(spectrogram)
+    for i in range(hp.n_iter):
+        X_t = invert_spectrogram(X_best)
+        est = librosa.stft(X_t, hp.n_fft, hp.hop_length, win_length=hp.win_length)
+        phase = est / np.maximum(1e-8, np.abs(est))
+        X_best = spectrogram * phase
+    X_t = invert_spectrogram(X_best)
+    y = np.real(X_t)
+
+    return y
+
+def invert_spectrogram(spectrogram):
+    '''Applies inverse fft.
+    Args:
+      spectrogram: [1+n_fft//2, t]
+    '''
+    return librosa.istft(spectrogram, hp.hop_length, win_length=hp.win_length, window="hann")
+
+def plot_alignment(alignment, gs, dir=hp.logdir):
+    """Plots the alignment.
+
+    Args:
+      alignment: A numpy array with shape of (encoder_steps, decoder_steps)
+      gs: (int) global step.
+      dir: Output path.
+    """
+    if not os.path.exists(dir): os.mkdir(dir)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(alignment)
+
+    fig.colorbar(im)
+    plt.title('{} Steps'.format(gs))
+    plt.savefig('{}/alignment_{}.png'.format(dir, gs), format='png')
+
+def guided_attention(g=0.2):
+    '''Guided attention. Refer to page 3 on the paper.'''
+    W = np.zeros((hp.max_N, hp.max_T), dtype=np.float32)
+    for n_pos in range(W.shape[0]):
+        for t_pos in range(W.shape[1]):
+            W[n_pos, t_pos] = 1 - np.exp(-(t_pos / float(hp.max_T) - n_pos / float(hp.max_N)) ** 2 / (2 * g * g))
+    return W
+
+def learning_rate_decay(init_lr, global_step, warmup_steps = 4000.0):
+    '''Noam scheme from tensor2tensor'''
+    step = tf.to_float(global_step + 1)
+    return init_lr * warmup_steps**0.5 * tf.minimum(step * warmup_steps**-1.5, step**-0.5)
+
+def load_spectrograms(fpath):
+    '''Read the wave file in `fpath`
+    and extracts spectrograms'''
+
+    fname = os.path.basename(fpath)
+    mel = get_spectrograms(fpath)
+    t = mel.shape[0]
+
+    # Marginal padding for reduction shape sync.
+    num_paddings = hp.r - (t % hp.r) if t % hp.r != 0 else 0
+    mel = np.pad(mel, [[0, num_paddings], [0, 0]], mode="constant")
+    
+
+    # Reduction
+    mel = mel[::hp.r, :]
+    return fname, mel
+

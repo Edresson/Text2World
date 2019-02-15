fbc1f784828bc11523aac6a560147c96811d7e5a
dresson <edresson1@gmail.com>
2019-02-15 08:07:10 -0200

    update

--- prepo.py
+++ prepo.py
@@ -61,8 +61,8 @@ if hp.phoneme == True:
 for fpath in tqdm.tqdm(fpaths):
     
         if not os.path.exists("worlds"): os.mkdir("worlds")
-        print(os.path.join(hp.data_dir,fpath))
-        world=wav2world(os.path.join(hp.data_dir,fpath))
+        print(fpath)
+        world=wav2world(fpath)
         '''num_padding = mel.shape[0]*8 - world.shape[0] 
         world = np.pad(world, [[0, num_padding], [0, 0]], mode="constant")'''
         np.save("worlds/{}".format(data_list[i].replace("wav", "npy")), world)
